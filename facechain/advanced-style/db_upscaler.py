20f9f009f869fee932f876e342c3700ddbb2b5fd
livehl@126.com <>
2023-08-21 17:21:03 +0800

    fix bug

--- facechain/advanced-style/db_upscaler.py
+++ facechain/advanced-style/db_upscaler.py
@@ -3,7 +3,14 @@ import torch
 from diffusers import StableDiffusionLatentUpscalePipeline, StableDiffusionUpscalePipeline
 
 
-def up_pic(prompt: str, img: Image):
+def up_4x_pic(prompt: str, img: Image):
+    upscaler = StableDiffusionUpscalePipeline.from_pretrained(
+        "system_lora/stable-diffusion-x4-upscaler", torch_dtype=torch.float16)
+    upscaler.to("cuda")
+    return upscaler(prompt=prompt, image=img).images[0]
+
+
+def up_2x_pic(prompt: str, img: Image):
     upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained(
         "system_lora/sd-x2-latent-upscaler", torch_dtype=torch.float16)
     upscaler.to("cuda")
